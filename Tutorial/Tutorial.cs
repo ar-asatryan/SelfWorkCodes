6a6c1c19944615193c7f55fdb5b85dbdec77b563
ar-asatryan <leoaroray@gmail.com>
2018-08-04 00:00:15 +0400

    Tutorial update

--- Tutorial/Tutorial.cs
+++ Tutorial/Tutorial.cs
@@ -8,7 +8,16 @@ namespace Tutorial
     {
         static void Main(string[] args)
         {
-
+            int inpNum = int.Parse(Console.ReadLine());
+            int revNum = 0;
+            int a;
+            while (inpNum > 0)
+            {
+                a = inpNum % 10;
+                revNum = revNum * 10 + a;
+                inpNum /= 10;
+            }
+            Console.WriteLine($"Reverse Number is {revNum}");
 
 
 
